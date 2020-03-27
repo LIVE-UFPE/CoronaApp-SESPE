98f0b57172f5975524346461a7e7f1470ea0751f
Job <ljba@cin.ufpe.br>
2020-03-27 14:23:38 -0300

    implementação de mais componentes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,13 @@
 <template>
   <v-container fluid class="primary d-flex flex-column justify-center align-center" fill-height>
-    <v-card raised color="secondary" max-width="470px" max-height="540px" width="470px" height="540px" fluid class="d-flex round-border">
-      
+    <v-card raised color="secondary" max-width="705px" max-height="809px" width="705px" height="809px" fluid class="d-flex round-border px-7 pt-12">
+      <v-row class="mx-12 mt-12 accent d-block" align="end" justify="center" > 
+        <p class="text-center font-weight-bold pb-12" v-bind:style="{fontSize: '60px',color: '#1E9CF8'}" >Logo</p> <!-- // ! Replacement for logo  -->
+        <!-- // TODO usar v-form? -->
+
+        <v-text-field placeholder="Email" outlined full-width class="pt-12" v-bind:style="{borderRadius: '10px'}" height="100px" single-line solo></v-text-field> 
+        <!-- // ! style deve ser usado da forma usada acima, ou camelCase ou 'kebab-case' : 'string' -->
+      </v-row>
     </v-card>
   </v-container>
 </template>
@@ -17,3 +23,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.v-text-field {
+  font-size: 30px;
+  border-radius: 10px;
+}
+</style>
